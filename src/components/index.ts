4f84f90c691f831c31b08f616ab363fa567ca57a
Tamires Manhaes <tamiresmanhaes4@gmail.com>
2024-04-14 13:38:36 -0300

    chore: adding new configs for publishing

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
+export { Alert } from './Alert';
 export { Badge } from './Badge';
 export { Button } from './Button';
+export { Checkbox } from './Checkbox';
+export { Select, SelectItem } from './Select';
+export { Switch } from './Switch';
 export { Tag } from './Tag';
+export { Textarea } from './Textarea';
