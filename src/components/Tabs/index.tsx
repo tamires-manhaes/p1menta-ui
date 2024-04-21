f7e54faa69894eaa8570759db1ce70aa57200fa4
Tamires Manhães <tamiresmanhaes4@gmail.com>
2024-04-21 19:16:38 -0300

    chore: finishing Tabs component

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
+      'inline-flex items-center justify-center whitespace-nowrap p-2 text-md text-neutral-500 font-medium border-b border-neutral-300 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-brand-500 data-[state=active]:border-b-2 data-[state=active]:border-brand-500 data-[state=active]:text-blue-500 data-[state=active]:text-4 data-[state=active]:font-medium',
       className,
     )}
     {...props}
