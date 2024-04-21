ec0f7574be607d48f2c9ef3b8e799d5c9ab34c0f
Tamires Manhães <tamiresmanhaes4@gmail.com>
2024-04-21 19:17:07 -0300

    feat: adding new components to export package

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,13 @@
 export { Alert } from './Alert';
+export { Avatar } from './Avatar'
 export { Badge } from './Badge';
 export { Button } from './Button';
 export { Checkbox } from './Checkbox';
+export { Input } from './Input';
+export { Modal } from './Modal';
+export { ProgressBar } from './ProgressBar'
 export { Select, SelectItem } from './Select';
 export { Switch } from './Switch';
+export { Tabs } from './Tabs';
 export { Tag } from './Tag';
 export { Textarea } from './Textarea';
