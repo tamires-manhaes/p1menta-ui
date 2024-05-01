cfbdc621af35699252ce162bf28895750e22fbc6
Tamires Manhães <tamiresmanhaes4@gmail.com>
2024-05-01 16:37:44 -0300

    fixing build problems

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,33 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/index.ts'),
+      name: '@p1menta-ui',
+      formats: ['cjs', 'es'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        banner: `'use client';`,
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
 });
